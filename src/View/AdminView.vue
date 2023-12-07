0998d3420bdd4a58c03722fb3957813dc30d862d
MplumYZ <70731447+MplumYZ@users.noreply.github.com>
2023-12-07 21:28:34 +0900

    2023.12.07
    更新adminAccount.js 为了说明vuex中函数调用接口导入的数据也包含在内

--- src/View/AdminView.vue
+++ src/View/AdminView.vue
@@ -1,17 +1,28 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: Meiyizhi
+ * @Date: 2023-05-18 17:04:44
+ * @LastEditTime: 2023-12-07 20:11:45
+-->
 <template>
     <div>
         this is adminView
     </div>
 </template>
 <script>
-import { adminAccountModule } from "../store/modules/adminAccount";
+import { adminAccountModule } from '../store/modules/adminAccount'
+import vuexLazyload from '../plugins/vuexLazyload'
 export default {
   name: 'adminView',
   mounted(){
-    this.$store.registerModule('admin', adminAccountModule)
-  },
-  beforeUnmount() {
-    this.$store.unregisterModule('admin', adminAccountModule)
-  },
+    vuexLazyload()
+  }
+  // mounted(){
+  //   this.$store.registerModule('admin', adminAccountModule)
+  // },
+  // beforeUnmount() {
+  //   this.$store.unregisterModule('admin', adminAccountModule)
+  // },
 }
 </script>
---
+++ src/store/modules/adminAccount.js
@@ -0,0 +1,29 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: Meiyizhi
+ * @Date: 2023-05-16 15:45:41
+ * @LastEditTime: 2023-12-07 20:00:21
+ */
+import axios from '../../plugins/request.js'
+
+const getUserApi = () => {
+	return axios({
+		url: '/api/admin/user',
+		method: 'post'
+	})
+}
+
+const mutations = {
+    getUser: (state) => {
+        Object.assign(state, getUserApi)
+    }
+}
+
+export default{
+    namespaced: true,
+    state:()=>({
+        msg: 'adminAccount'
+    }),
+    mutations
+}
